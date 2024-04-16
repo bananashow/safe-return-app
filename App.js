7c4b6640646ff752f58da7f62d5b141c4fb6e2d0
bananaqick <bananaqick@gmail.com>
2024-04-16 17:21:21 +0900

    chore : axios 설치

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 import { Navigator } from './src/components/navigate/Navigator';
-import { SAFE_API_KEY } from '@env';
 
 export default function App() {
-  console.log('SAFE_API_KEY : ', SAFE_API_KEY);
   return <Navigator />;
 }
