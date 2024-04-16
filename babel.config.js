3df630f1aed4bf18eab076bbbf5b00886252609c
bananaqick <bananaqick@gmail.com>
2024-04-16 17:11:21 +0900

    chore : config 설정

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,17 @@
 module.exports = function (api) {
-  api.cache(false);
-  module.exports = {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
     plugins: [
       [
         'module:react-native-dotenv',
         {
-          envName: 'APP_ENV',
           moduleName: '@env',
           path: '.env',
-          blocklist: null,
-          allowlist: null,
-          blacklist: null, // DEPRECATED
-          whitelist: null, // DEPRECATED
+          blacklist: null,
+          whitelist: null,
           safe: false,
           allowUndefined: true,
-          verbose: false,
         },
       ],
     ],
