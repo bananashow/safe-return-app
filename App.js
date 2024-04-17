6b07716c602bd515b0ba6c283b62a026e4fd1294
bananaqick <bananaqick@gmail.com>
2024-04-17 11:43:29 +0900

    feat : react-query 세팅

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
+import { QueryClientProvider } from 'react-query';
 import { Navigator } from './src/components/navigate/Navigator';
+import { queryClient } from './src/api/queryClient';
 
 export default function App() {
-  return <Navigator />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Navigator />
+    </QueryClientProvider>
+  );
 }
